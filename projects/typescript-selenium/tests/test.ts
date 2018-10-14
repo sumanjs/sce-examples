cd1f85d5c96ae9232d97aa452e979d6164a5e539
Tim Mensch <tim-sg-chimera@bitgems.com>
2018-10-14 10:50:37 -0600

    Updated path to import
    * Also deleted lots of dead comment/code.

--- projects/typescript-selenium/tests/test.ts
+++ projects/typescript-selenium/tests/test.ts
@@ -1,56 +1,6 @@
 import "chromedriver";
 import { By } from "selenium-webdriver";
-import { SCETestHelper } from "@sce/code-generation";
-
-// describe("Submit ideas", () => {
-//   let pages: AllPages;
-
-//   before(async () => {
-//     pages = new AllPages(new Browser("chrome"));
-//   });
-
-//   it("Test Case #1: Unauthenticated cannot submit ideas", async () => {
-//     // Action
-//     await pages.home.navigate();
-//     await pages.home.IdeaTitle.type("Add support to TypeScript");
-
-//     // Assert
-//     await Promise.all([
-//       ensure(pages.home.UserMenu).textIs("Sign in"),
-//       ensure(pages.home.SubmitIdea).isNotVisible(),
-//       ensure(pages.home.IdeaDescription).isNotVisible(),
-//     ]);
-//   });
-
-//   it("Test Case #2: Authenticated can submit ideas", async () => {
-//     // Action
-//     await pages.home.navigate();
-//     await pages.home.signInWithGoogle();
-//     await pages.google.signInAsDarthVader();
-
-//     // Assert
-//     await ensure(pages.home.UserMenu).textIs("Darth Vader");
-
-//     // Action
-//     await pages.home.submitNewIdea(
-//       "Host a TypeScript workshop!",
-//       "Workshop would be useful to have hands-on practice with the language."
-//     );
-
-//     // Assert
-//     await Promise.all([
-//       ensure(pages.showIdea.Title).textIs("Host a TypeScript workshop!"),
-//       ensure(pages.showIdea.Description).textIs(
-//         "Workshop would be useful to have hands-on practice with the language."
-//       ),
-//       ensure(pages.showIdea.SupportCounter).textIs("1"),
-//     ]);
-//   });
-
-//   after(async () => {
-//     await pages.dispose();
-//   });
-// });
+import { SCETestHelper } from "@sce/code-generation/dist/typescriptHelpers";
 
 // Tool will output a single top-level "describe" block
 describe("SCE Test Session", () => {
